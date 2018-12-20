aef47ce6dcc91e902dd14054c189627aabdb69db
Mohammad Talha Khan <mohammad.talha@Talha.local>
2018-12-21 02:24:54 +0530

    Pattern Matching added

--- src/main/java/flipkart/retail/temp/MRPScan.java
+++ src/main/java/flipkart/retail/temp/MRPScan.java
@@ -15,13 +15,18 @@ import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.cloud.vision.v1.TextAnnotation;
 import com.google.protobuf.ByteString;
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Created by mohammad.talha on 20/12/18.
@@ -33,42 +38,50 @@ public class MRPScan {
     try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
       // The path to the image file to annotate
-      String fileName = "/Users/mohammad.talha/Downloads/a1.jpeg";
-
-      // Reads the image file into memory
+      List<AnnotateImageRequest> requests = new ArrayList<>();
+      String baseFile = "/Users/mohammad.talha/Downloads/fileText_3";
+      for (int i = 11; i <= 14; i++) {
+        String fileName = "/Users/mohammad.talha/Downloads/next_3/a" + i + ".jpeg";
 //      export GOOGLE_APPLICATION_CREDENTIALS="/Users/mohammad.talha/Downloads/key.json";
 
+        Path path = Paths.get(fileName);
+        byte[] data = Files.readAllBytes(path);
+        ByteString imgBytes = ByteString.copyFrom(data);
 
-      Path path = Paths.get(fileName);
-      byte[] data = Files.readAllBytes(path);
-      ByteString imgBytes = ByteString.copyFrom(data);
+        // Builds the image annotation request
 
-      // Builds the image annotation request
-      List<AnnotateImageRequest> requests = new ArrayList<>();
-      Image img = Image.newBuilder().setContent(imgBytes).build();
-      Feature feat = Feature.newBuilder().setType(Type.DOCUMENT_TEXT_DETECTION).build();
-      AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-          .addFeatures(feat)
-          .setImage(img)
-          .build();
-      requests.add(request);
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Type.DOCUMENT_TEXT_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+            .addFeatures(feat)
+            .setImage(img)
+            .build();
+        requests.add(request);
+      }
 
       // Performs label detection on the image file
 
-      long st = System.currentTimeMillis();
+//      long st = System.currentTimeMillis();
       BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
       List<AnnotateImageResponse> responses = response.getResponsesList();
+      int c = 11;
       for (AnnotateImageResponse res : responses) {
         if (res.hasError()) {
           System.out.printf("Error: %s\n", res.getError().getMessage());
           return;
         }
-
         TextAnnotation textAnnotation = res.getFullTextAnnotation();
-//        No serializer found for class com.google.protobuf.UnknownFieldSet$Parser and no properties discovered to create BeanSerializer (to avoid exception, disable SerializationFeature.FAIL_ON_EMPTY_BEANS)
-        System.out.println(textAnnotation.getText());
-
+        String text = textAnnotation.getText();
+        text = text.replace("\n", " ");
+        System.out.println(text);
+        File fout = new File(baseFile + "/a" + c++ + ".csv");
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        bw.write(text);
+        bw.newLine();
+        bw.close();
       }
     }
   }
 }
+
---
+++ src/main/java/flipkart/retail/temp/ReadFile.java
@@ -0,0 +1,82 @@
+package flipkart.retail.temp;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by mohammad.talha on 20/12/18.
+ */
+public class ReadFile {
+
+  public String findMrp(String text) {
+    String res = "";
+//text="INNOVATION SILK PROTEIN 420 ab 145/-.00.1450 ook more moothman Proven result 10% SMOOTH The re and shine of your hori Your hair is smooth, controlled and Follow the L'Oréal Paris SMOOTH INTENSE progn SHAMPOO CONDITIONER 3 varme Condition Detangle IN CASE OF CONTACT WITH EYES. RINSE IMMEDIATELY OP 10 LEI Mktd. by / Regd. office: L'Oréal India Pvt. Ltd. A-Wing, 8th Floor, Marathon Futurex, N. M. Joshi Mara. Lower Parel, Mumbai - 400013. Any questions/complaints? Call or write: Consumer Advisor, at Regd, office; Tel: 1800-22-3000; E-mail: Advisor.India@loreal.com 175ml+17.5ml FREE = 192.5ml B515791C 16-03/2018 15-02/2021 Rs. 150 10 TOATE LOVE MEBLE 81901526 10135 1> \n ";
+
+    String NUM_DOT_OO = "(\\d+\\.00)";
+    String NUM_SLASH_HYPHERN = "(\\d+)\\/-";
+    String ANY_CHAR_NUM_OPTIONAL_00 = ".*?(\\d+\\.?0?0?)";
+    String ANY_CHAR_NUM_SLASH_HYPHEN = ".*?(\\d+)\\/-";
+    String RS = "Rs";
+    String MRP = "MRP";
+    String MAXIMUM_RETAIL_PRICE = "MAXIMUM RETAIL PRICE";
+    String M_DOT_R_DOT_P_DOT = "M.R.P.";
+
+    Pattern p1 = Pattern.compile(NUM_SLASH_HYPHERN);
+    Pattern p2 = Pattern.compile(NUM_DOT_OO);
+    Pattern p3 = Pattern.compile(RS + ANY_CHAR_NUM_OPTIONAL_00);
+
+    Pattern p4 = Pattern.compile(MRP + ANY_CHAR_NUM_SLASH_HYPHEN, Pattern.CASE_INSENSITIVE);
+    Pattern p5 = Pattern
+        .compile(MAXIMUM_RETAIL_PRICE + ANY_CHAR_NUM_SLASH_HYPHEN, Pattern.CASE_INSENSITIVE);
+    Pattern p6 = Pattern
+        .compile(M_DOT_R_DOT_P_DOT + ANY_CHAR_NUM_SLASH_HYPHEN, Pattern.CASE_INSENSITIVE);
+
+    Pattern p7 = Pattern.compile(MRP + ANY_CHAR_NUM_OPTIONAL_00, Pattern.CASE_INSENSITIVE);
+    Pattern p8 = Pattern
+        .compile(MAXIMUM_RETAIL_PRICE + ANY_CHAR_NUM_OPTIONAL_00, Pattern.CASE_INSENSITIVE);
+    Pattern p9 = Pattern
+        .compile(M_DOT_R_DOT_P_DOT + ANY_CHAR_NUM_OPTIONAL_00, Pattern.CASE_INSENSITIVE);
+
+    List<Pattern> patterns = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9);
+
+    for (Pattern pattern : patterns) {
+      Matcher m = pattern.matcher(text);
+      if (m.find()) {
+        res = m.group(1);
+        break;
+      }
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) throws Exception {
+    String baseFile = "/Users/mohammad.talha/Downloads/fileText_2";
+    int c = 0;
+    for (int i = 1; i <= 14; i++) {
+      String file = baseFile + "/a" + i + ".csv";
+      Scanner scanner = new Scanner(new File(file));
+      while (scanner.hasNext()) {
+        String text = scanner.nextLine();
+        ReadFile readFile = new ReadFile();
+        String mrp = readFile.findMrp(text);
+        if (mrp.length() != 0 && (mrp.charAt(0) == '2' || mrp.charAt(0) == '3')) {
+          Double d = Double.parseDouble(mrp);
+          if (d > 1999.0) {
+            mrp = mrp.substring(1);
+          }
+        }
+        System.out.println("i = " + i + " " + mrp);
+      }
+    }
+  }
+
+
+}
